a8e31fe57f69ca85cfc5466868734073708f5769
Vitor Natario <natariovitor@gmail.com>
2023-09-04 18:21:58 -0300

    styles: add DeafultLayout

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/Home'
 import { CheckoutPage } from './pages/Checkout'
+import { DefaultLayout } from './layouts/Default'
 
 export function Router() {
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/checkout' element={<CheckoutPage />} />
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
+      </Route>
     </Routes>
   )
 }
