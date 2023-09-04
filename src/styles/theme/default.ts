893f7c018e818536dcfae4dfa5b483631445dddc
Vitor Natario <natariovitor@gmail.com>
2023-09-04 16:37:25 -0300

    styles: add defaultTheme

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,42 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+    background: '#FAFAFA',
+    'base-card': '#F3F2F2',
+    'base-input': '#EDEDED',
+    'base-button': '#E6E5E5',
+    'base-hover': '#D7D5D5',
+    'base-label': '#8D8686',
+    'base-text': '#574F4D',
+    'base-subtitle': '#403937',
+    'base-title': '#272221',
+
+    'purple-light': '#EBE5F9',
+    purple: '#8047F8',
+    'purple-dark': '#4B2995',
+
+    'yellow-light': '#F1E9C9',
+    yellow: '#DBAC2C',
+    'yellow-dark': '#C47F17',
+
+    error: '#F84747',
+  },
+  textSizes: {
+    'title-xl': '3rem',
+    'title-l': '2rem',
+    'title-m': '1.5rem',
+    'title-s': '1.25rem',
+    'title-xs': '1.125rem',
+    'text-l': '1.25rem',
+    'text-m': '1rem',
+    'text-s': '0.875rem',
+    'text-xs': '0.75rem',
+    tag: '0.625rem',
+    'button-g': '0.875rem',
+    'button-m': '0.75rem',
+  },
+  fonts: {
+    regular: "'Roboto'",
+    title: "'Baloo 2'",
+  },
+}
