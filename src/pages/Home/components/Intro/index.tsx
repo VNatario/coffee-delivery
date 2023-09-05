b8049ae8efdfbcd78a2881af432c30257c547ad3
Vitor Natario <natariovitor@gmail.com>
2023-09-05 17:26:27 -0300

    syle: add background in IntroContainer

--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -3,7 +3,7 @@ import { IntroContainer, IntroContent } from './styles'
 export function Intro() {
   return (
     <IntroContainer>
-      <IntroContent>
+      <IntroContent className='container'>
         <p>Teste</p>
       </IntroContent>
     </IntroContainer>
--- src/pages/Home/components/Intro/styles.ts
+++ src/pages/Home/components/Intro/styles.ts
@@ -4,8 +4,19 @@ import introBackgroundImg from '../../../../assets/intro-background.svg'
 export const IntroContainer = styled.section`
   width: 100%;
   height: 34rem;
-  background: ${() => css`url(${introBackgroundImg}) no-repeat center `};
-  background-size: cover;
+
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    background: ${() => css`url(${introBackgroundImg}) no-repeat center `};
+    background-size: cover;
+    filter: blur(80px);
+  }
 `
 
 export const IntroContent = styled.div`
