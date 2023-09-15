0430c04fed019e0e569ee2ef7e03e38be8965a18
Vitor Natario <natariovitor@gmail.com>
2023-09-15 18:25:23 -0300

    style: add navigation in headerComponent

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,21 +2,26 @@ import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
 
 import coffeeLogo from '../../assets/coffee-delivery-logo.svg'
 import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <div className='container'>
-        <img src={coffeeLogo} />
+        <NavLink to='/'>
+          <img src={coffeeLogo} />
+        </NavLink>
 
         <HeaderButtonsContainer>
           <HeaderButton $variant='purple'>
             <MapPin size={20} weight='fill' />
             Porto Alegre, RS
           </HeaderButton>
-          <HeaderButton $variant='yellow'>
-            <ShoppingCart size={20} weight='fill' />
-          </HeaderButton>
+          <NavLink to='/checkout'>
+            <HeaderButton $variant='yellow'>
+              <ShoppingCart size={20} weight='fill' />
+            </HeaderButton>
+          </NavLink>
         </HeaderButtonsContainer>
       </div>
     </HeaderContainer>
