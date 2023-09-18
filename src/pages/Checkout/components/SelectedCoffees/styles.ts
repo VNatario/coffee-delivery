5d4d274885f980be79e5dbc80f89a28fdd0ad3a3
Vitor Natario <natariovitor@gmail.com>
2023-09-18 18:26:15 -0300

    style: create SelectedCoffees

---
+++ src/pages/Checkout/components/SelectedCoffees/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { SectionBasedStyle } from '../../styles'
+
+export const SelectedCoffeesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  width: 28rem;
+`
+
+export const DetailsContainer = styled(SectionBasedStyle)`
+  border-radius: 6px 44px;
+  display: flex;
+  flex-direction: column;
+`
--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,10 +1,12 @@
 import { CompleteOrderForm } from './components/CompleteOrderForm'
+import { SelectedCoffees } from './components/SelectedCoffees'
 import { CheckoutContainer } from './styles'
 
 export function CheckoutPage() {
   return (
     <CheckoutContainer className='container'>
       <CompleteOrderForm />
+      <SelectedCoffees />
     </CheckoutContainer>
   )
 }
