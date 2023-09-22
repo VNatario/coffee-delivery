b288ef82b04edf8149a365b69945d3101f3f16a2
Vitor Natario <natariovitor@gmail.com>
2023-09-22 16:33:36 -0300

    feat: create validation form and typing form

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,12 +1,38 @@
 import { CompleteOrderForm } from './components/CompleteOrderForm'
 import { SelectedCoffees } from './components/SelectedCoffees'
 import { CheckoutContainer } from './styles'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm, FormProvider } from 'react-hook-form'
+
+const confirmOrderFormValidationSchema = zod.object({
+  cep: zod.string(),
+})
+
+export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>
+
+type ConfirmOrderFormData = OrderData
 
 export function CheckoutPage() {
+  const confirmOrderForm = useForm({
+    resolver: zodResolver(confirmOrderFormValidationSchema),
+  })
+
+  const { handleSubmit } = confirmOrderForm
+
+  function handleConfirmOrder(data: ConfirmOrderFormData) {
+    console.log(data)
+  }
+
   return (
-    <CheckoutContainer className='container'>
-      <CompleteOrderForm />
-      <SelectedCoffees />
-    </CheckoutContainer>
+    <FormProvider {...confirmOrderForm}>
+      <CheckoutContainer
+        className='container'
+        onSubmit={handleSubmit(handleConfirmOrder)}
+      >
+        <CompleteOrderForm />
+        <SelectedCoffees />
+      </CheckoutContainer>
+    </FormProvider>
   )
 }
